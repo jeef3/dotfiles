12aadbc054bfb7d4b0025ae2cd2888881c881528
Jeff Knaggs <mail@jeef3.com>
2013-11-09 17:01:05 +1300

    Added some of my base set-up

---
+++ install-deps.sh
@@ -0,0 +1,16 @@
+# TODO:
+# rbenv vs rvm
+
+# homebrew
+ruby <(curl -fsSkL raw.github.com/mxcl/homebrew/go)
+
+# ----------
+# Tools
+
+mkdir ~/tools
+cd ~/tools
+
+# https://github.com/rupa/z
+# For jumping around dirs
+git clone https://github.com/rupa/z.git
+chmod +x z/z.sh
