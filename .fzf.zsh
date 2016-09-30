2215920a51ddb02deeb540f8451dc6af48fbddca
Jeff Knaggs <mail@jeef3.com>
2016-09-30 22:13:28 +1300

    Some config updates

--- .fzf.zsh
+++ .fzf.zsh
@@ -1,20 +1,20 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
-  export PATH="$PATH:/usr/local/opt/fzf/bin"
+if [[ ! "$PATH" == */Users/jeffknaggs/.fzf/bin* ]]; then
+  export PATH="$PATH:/Users/jeffknaggs/.fzf/bin"
 fi
 
 # Man path
 # --------
-if [[ ! "$MANPATH" == */usr/local/opt/fzf/man* && -d "/usr/local/opt/fzf/man" ]]; then
-  export MANPATH="$MANPATH:/usr/local/opt/fzf/man"
+if [[ ! "$MANPATH" == */Users/jeffknaggs/.fzf/man* && -d "/Users/jeffknaggs/.fzf/man" ]]; then
+  export MANPATH="$MANPATH:/Users/jeffknaggs/.fzf/man"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "/Users/jeffknaggs/.fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/usr/local/opt/fzf/shell/key-bindings.zsh"
+source "/Users/jeffknaggs/.fzf/shell/key-bindings.zsh"
 
