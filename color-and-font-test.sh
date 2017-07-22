3c7eb06172fd7c071945ed4e93c3baade1d3bc97
Jeff Knaggs <mail@jeef3.com>
2017-07-22 13:53:56 +1200

    Add script used to check colors and fonts are work

---
+++ color-and-font-test.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+printf "\n\n     "
+for j in {0..8}; do
+  printf "${j}    "
+done
+
+for j in {0..5}; do
+  printf "\n\n ${j}   "
+  for i in {0..8}; do
+    echo -en "\033[38;${j};${i}mfoo\033[m  "
+  done
+done
+
+echo -e "\n"
+
+awk 'BEGIN{
+    s="               "; s=s s s s s s s s;
+    for (colnum = 0; colnum<77; colnum++) {
+        r = 255-(colnum*255/76);
+        g = (colnum*510/76);
+        b = (colnum*255/76);
+        if (g>255) g = 510-g;
+        printf "\033[48;2;%d;%d;%dm", r,g,b;
+        printf "\033[38;2;%d;%d;%dm", 255-r,255-g,255-b;
+        printf "%s\033[0m", substr(s,colnum+1,1);
+    }
+    printf "\n";
+}'
+
+echo -e "\n"
