95cb1dd5c4abce656c8ea07c1d0dbc07ee57296b
Jeff Knaggs <mail@jeef3.com>
2018-08-31 09:52:00 +1200

    Add some zhush ✨

--- .zsh/prompt/current-dir.zsh
+++ .zsh/prompt/current-dir.zsh
@@ -2,10 +2,11 @@ FOLDER_ICON="\uf07c"
 ARROW="\ue0b0"
 ANGLE="\ue0b0"
 
-START_SEGMENT=$(hex_to_true 999999 484848)
-END_SEGMENT=$(hex_to_true 484848)
+START_SEGMENT=$(hex_to_true 999999 555555)
+END_SEGMENT=$(hex_to_true 555555 444444)
+TAIL=$(hex_to_true 444444 000000)
 
-FOLDER="${START_SEGMENT} ${FOLDER_ICON} ${RESET}${END_SEGMENT}${ANGLE}${RESET} "
+FOLDER="${START_SEGMENT} ${FOLDER_ICON} ${RESET}${END_SEGMENT}${ANGLE}${RESET}${TAIL}${ANGLE} "
 
 current_dir() {
   typeset collapsed_dir=$(pwd | perl -pe '
