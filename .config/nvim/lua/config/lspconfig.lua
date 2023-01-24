d41d48844b5dc5638ef7b666438f42f3ea978a0f
Jeff Knaggs <mail@jeef3.com>
2023-01-25 11:42:28 +1300

    Disable those for now

--- .config/nvim/lua/config/lspconfig.lua
+++ .config/nvim/lua/config/lspconfig.lua
@@ -99,8 +99,8 @@ null_ls.setup({
     -- null_ls.builtins.diagnostics.luacheck,
     null_ls.builtins.code_actions.eslint_d,
     null_ls.builtins.formatting.prettierd,
-    null_ls.builtins.formatting.astyle,
-    null_ls.builtins.formatting.stylua,
+    -- null_ls.builtins.formatting.astyle,
+    -- null_ls.builtins.formatting.stylua,
   },
   on_attach = on_attach,
 })
