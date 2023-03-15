017a3a83aea050cba7997bdfe9f200926594c202
Jeff Knaggs <mail@jeef3.com>
2023-03-15 15:18:49 +1300

    Hacky fix for csharp

--- .config/nvim/lua/config/lspconfig.lua
+++ .config/nvim/lua/config/lspconfig.lua
@@ -25,7 +25,7 @@ lspsaga.setup({
     code_action = "💡",
     hover = " ",
   },
-  symbol_in_winbar = { enable = true, separator = "  " },
+  symbol_in_winbar = { enable = false, separator = "  " },
 })
 
 local lsp_formatting = function(bufnr)
@@ -112,6 +112,84 @@ local on_attach = function(client, bufnr)
       end,
     })
   end
+
+  -- Workaround for broken OmniSharp
+  if client.name == "omnisharp" then
+    client.server_capabilities.semanticTokensProvider = {
+      full = vim.empty_dict(),
+      legend = {
+        tokenModifiers = { "static_symbol" },
+        tokenTypes = {
+          "comment",
+          "excluded_code",
+          "identifier",
+          "keyword",
+          "keyword_control",
+          "number",
+          "operator",
+          "operator_overloaded",
+          "preprocessor_keyword",
+          "string",
+          "whitespace",
+          "text",
+          "static_symbol",
+          "preprocessor_text",
+          "punctuation",
+          "string_verbatim",
+          "string_escape_character",
+          "class_name",
+          "delegate_name",
+          "enum_name",
+          "interface_name",
+          "module_name",
+          "struct_name",
+          "type_parameter_name",
+          "field_name",
+          "enum_member_name",
+          "constant_name",
+          "local_name",
+          "parameter_name",
+          "method_name",
+          "extension_method_name",
+          "property_name",
+          "event_name",
+          "namespace_name",
+          "label_name",
+          "xml_doc_comment_attribute_name",
+          "xml_doc_comment_attribute_quotes",
+          "xml_doc_comment_attribute_value",
+          "xml_doc_comment_cdata_section",
+          "xml_doc_comment_comment",
+          "xml_doc_comment_delimiter",
+          "xml_doc_comment_entity_reference",
+          "xml_doc_comment_name",
+          "xml_doc_comment_processing_instruction",
+          "xml_doc_comment_text",
+          "xml_literal_attribute_name",
+          "xml_literal_attribute_quotes",
+          "xml_literal_attribute_value",
+          "xml_literal_cdata_section",
+          "xml_literal_comment",
+          "xml_literal_delimiter",
+          "xml_literal_embedded_expression",
+          "xml_literal_entity_reference",
+          "xml_literal_name",
+          "xml_literal_processing_instruction",
+          "xml_literal_text",
+          "regex_comment",
+          "regex_character_class",
+          "regex_anchor",
+          "regex_quantifier",
+          "regex_grouping",
+          "regex_alternation",
+          "regex_text",
+          "regex_self_escaped_character",
+          "regex_other_escape",
+        },
+      },
+      range = true,
+    }
+  end
 end
 
 null_ls.setup({
@@ -162,16 +240,16 @@ lspconfig.sourcekit.setup({
   on_attach = on_attach,
 })
 
--- local pid = vim.fn.getpid()
--- local omnisharp_bin = "/Users/jeffknaggs/.local/share/nvim/mason/bin/OmniSharp"
+local omnisharp_bin = "/Users/jeffknaggs/.local/share/nvim/mason/bin/omnisharp"
 
--- lspconfig.omnisharp.setup({
---   cmd = {
---     omnisharp_bin,
---     "--languageserver",
---     -- "--hostPID",
---     -- tostring(pid)
---   },
+lspconfig.omnisharp.setup({
+  cmd = { omnisharp_bin },
+  capabilities = capabilities,
+  on_attach = on_attach,
+})
+
+-- lspconfig.csharp_ls.setup({
+--   cmd = { "/Users/jeffknaggs/.local/share/nvim/mason/bin/csharp-ls" },
 --   capabilities = capabilities,
 --   on_attach = on_attach,
 -- })
