d8845db2d7bd641c3a28ab661d96cf4c11c57f53
Jeff Knaggs <mail@jeef3.com>
2023-05-05 16:23:15 +1200

    Updating fonts

--- patch-font.sh
+++ patch-font.sh
@@ -7,7 +7,9 @@ success() { printf "\r\033[2K  [ \033[00;32mOK\033[0m ] $1\n"; }
 fail() { printf "\r\033[2K  [\033[0;31mFAIL\033[0m] $1\n"; exit; }
 
 NERD_FONT_PATH="$HOME/.tmp/nerd-fonts"
-FONT_PATH="$HOME/Library/Mobile Documents/com~apple~CloudDocs/Fonts/OperatorMono"
+OPERATOR_MONO_LIG_PATH="$HOME/.tmp/operator-mono-lig"
+
+FONT_PATH="$HOME/Library/Mobile Documents/com~apple~CloudDocs/Fonts/OperatorMono For Patch"
 
 if [ -e "$NERD_FONT_PATH" ]; then
   info "Nerd Fonts already downloaded, updating..."
@@ -15,24 +17,43 @@ if [ -e "$NERD_FONT_PATH" ]; then
 else
   info "Nerd Fonts not found, cloning..."
   # mkdir -p .tmp
-  git clone git@github.com:ryanoasis/nerd-fonts.git $NERD_FONT_PATH
+  git clone --depth=1 git@github.com:ryanoasis/nerd-fonts.git $NERD_FONT_PATH
 fi
 
-success "Nerd Fonts up-to-date"
-info "Patching font..."
+if [ -e "$OPERATOR_MONO_LIG_PATH" ]; then
+  info "Operator Mono Lig already downloaded, updating..."
+  cd $OPERATOR_MONO_LIG_PATH&& git pull
+else
+  info "Operator Mono Lig not found, cloning..."
+  git clone --depth=1 git@github.com:kiliman/operator-mono-lig.git $OPERATOR_MONO_LIG_PATH
+
+  cd $OPERATOR_MONO_LIG_PATH
+  npm install
+fi
+
+success "Nerd Fonts and Operator Mono Lig up-to-date"
 
 fonts=(
-  OperatorMono-Bold
-  OperatorMono-BoldItalic
   OperatorMono-Book
   OperatorMono-BookItalic
-  OperatorMono-Light
-  OperatorMono-LightItalic
-  OperatorMono-Medium
-  OperatorMono-MediumItalic
-  OperatorMono-XLight
-  OperatorMono-XLightItalic
+  OperatorMonoSSm-Bold
+  OperatorMonoSSm-BoldItalic
 )
+
+info "Adding ligatures"
+cd "$FONT_PATH"
+cp *.otf "$OPERATOR_MONO_LIG_PATH/original"
+cd "$OPERATOR_MONO_LIG_PATH"
+./build.sh
+
+fonts=(
+  OperatorMonoLig-Book
+  OperatorMonoLig-BookItalic
+  OperatorMonoSSmLig-Bold
+  OperatorMonoSSmLig-BoldItalic
+)
+
+info "Patching font..."
 
 cd $NERD_FONT_PATH
 
@@ -40,8 +61,9 @@ for font in ${fonts[@]}
 do
   info "Patch and install ${font}"
 
-  ./font-patcher "$FONT_PATH/${font}.otf" \
-    --mono \
+  ./font-patcher "$OPERATOR_MONO_LIG_PATH/build/${font}.otf" \
+    --quiet \
+    --careful \
     --complete \
     --out "$HOME/Desktop"
 
