eab0a1384d3678cc895dd71607333bcc6e1c9cae
Jeff Knaggs <mail@jeef3.com>
2024-03-28 10:24:08 +1300

    Tweak my hl indicator

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -37,34 +37,18 @@ return {
   {
     "kevinhwang91/nvim-hlslens",
     opts = {
-      override_lens = function(render, posList, nearest, idx, relIdx)
-        local sfw = vim.v.searchforward == 1
-        local indicator, text, chunks
-        local absRelIdx = math.abs(relIdx)
-        if absRelIdx > 1 then
-          indicator = ("%d%s"):format(
-            absRelIdx,
-            sfw ~= (relIdx > 1) and "▲" or "▼"
-          )
-        elseif absRelIdx == 1 then
-          indicator = sfw ~= (relIdx == 1) and "▲" or "▼"
-        else
-          indicator = ""
-        end
-
+      override_lens = function(render, posList, nearest, idx, _)
         local lnum, col = unpack(posList[idx])
+        local cnt = #posList
+        local text = ("←  %d of %d"):format(idx, cnt)
+        local chunks
+
         if nearest then
-          local cnt = #posList
-          if indicator ~= "" then
-            text = ("[%s %d/%d]"):format(indicator, idx, cnt)
-          else
-            text = ("[%d/%d]"):format(idx, cnt)
-          end
           chunks = { { " " }, { text, "HlSearchLensNear" } }
         else
-          text = ("[%s %d]"):format(indicator, idx)
           chunks = { { " " }, { text, "HlSearchLens" } }
         end
+
         render.setVirt(0, lnum - 1, col - 1, chunks, nearest)
       end,
     },
