4169d080e226ca4504023dc6abb493f7bf71c8c9
Jeff Knaggs <mail@jeef3.com>
2024-04-19 14:22:03 +1200

    Remove tmux addin
    Not using it, prefer proper Tmux switching

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -33,24 +33,6 @@ return {
   },
 
   {
-    "christoomey/vim-tmux-navigator",
-    cmd = {
-      "TmuxNavigateLeft",
-      "TmuxNavigateDown",
-      "TmuxNavigateUp",
-      "TmuxNavigateRight",
-      "TmuxNavigatePrevious",
-    },
-    keys = {
-      { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
-      { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
-      { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
-      { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
-      { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
-    },
-  },
-
-  {
     "kevinhwang91/nvim-hlslens",
     opts = {
       override_lens = function(render, posList, nearest, idx, _)
