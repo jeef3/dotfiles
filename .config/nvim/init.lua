df075913a86fb08e82a9822fc429ec9420b3f8bd
Jeff Knaggs <mail@jeef3.com>
2025-01-08 10:04:42 +1300

    Update nvim lazy install

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -8,22 +8,10 @@ vim.g.maplocalleader = "\\"
 -- Lazy
 --
 -- Plug in management
-
 vim.opt.termguicolors = true
 vim.opt.background = "dark"
 
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
+require("config.lazy")
 require("lazy").setup("plugins")
 
 ----------------
