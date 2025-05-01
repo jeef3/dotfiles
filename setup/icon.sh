c044c06348dd60e0f3aee298924395b0d0099637
Jeff Knaggs <mail@jeef3.com>
2025-05-01 12:26:28 +1200

    Maybe putting tmux back...

---
+++ setup/icon.sh
@@ -0,0 +1,17 @@
+get_icon() {
+  case "$1" in
+    bash) echo "" ;;
+    btop) echo "󰄧" ;;
+    brew) echo "" ;;
+    node) echo "" ;;
+    nvim) echo "" ;;
+    python) echo "" ;;
+    Python) echo "" ;;
+    ranger) echo "" ;;
+    ruby) echo "" ;;
+    vim) echo "" ;;
+    Vim) echo "" ;;
+    yazi) echo "" ;;
+    zsh) echo "" ;;
+  esac
+}
