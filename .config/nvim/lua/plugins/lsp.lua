7eadf6f4c0acfc8dffc6c54360ff686c2afa37da
Jeff Knaggs <mail@jeef3.com>
2025-06-21 16:44:28 +1200

    Remove nvim lint

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -164,34 +164,6 @@ return {
     },
   },
 
-  ----------------
-  -- Nvim Lint
-  --
-  -- An asynchronous linter plugin for Neovim complementary to the built-in
-  -- Language Server Protocol support.
-  --
-  -- https://github.com/mfussenegger/nvim-lint
-  ----------------
-  {
-    "mfussenegger/nvim-lint",
-    enabled = false,
-    config = function()
-      require("lint").linters_by_ft = tools.lint_filetypes
-
-      vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-        callback = function()
-          -- try_lint without arguments runs the linters defined in `linters_by_ft`
-          -- for the current filetype
-          require("lint").try_lint()
-
-          -- You can call `try_lint` with a linter name or a list of names to always
-          -- run specific linters, independent of the `linters_by_ft` configuration
-          require("lint").try_lint("cspell")
-        end,
-      })
-    end,
-  },
-
   {
     "MeanderingProgrammer/render-markdown.nvim",
     dependencies = {
