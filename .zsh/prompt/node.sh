291d6df8588f7fce21bddac130f458e731e0c4e6
Jeff Knaggs <mail@jeef3.com>
2026-04-28 10:52:53 +1200

    Updated version checking logic

--- .zsh/prompt/node.sh
+++ .zsh/prompt/node.sh
@@ -1,11 +1,20 @@
 function node_version() {
   if command -v nvm &> /dev/null; then
-    # Traverse up the directory tree
     local dir=$(pwd)
     while [ "$dir" != "/" ]; do
       if [ -f "$dir/.nvmrc" ]; then
-        # If .nvmrc is found, print the current Node version
-        echo "${GREEN} ${DARK_GRAY}$(node -v)"
+        local expected=$(< "$dir/.nvmrc")
+        local current=$(node -v 2>/dev/null)
+
+        local expected_clean=${expected#v}
+        local current_clean=${current#v}
+
+        if [[ "$current_clean" != "$expected_clean"* ]]; then
+          echo "${GREEN} ${MAGENTA}${BOLD}${current}${RESET}${DARK_GRAY} (v${expected_clean})"
+        else
+          echo "${GREEN} ${DARK_GRAY}${current}"
+        fi
+
         return
       fi
       dir=$(dirname "$dir")
