2934122d02e383943a0f38072ae0db57de129dde
Jeff Knaggs <mail@jeef3.com>
2026-04-29 13:04:23 +1200

    Tidy up prompt

--- .zsh/prompt.zsh
+++ .zsh/prompt.zsh
@@ -11,44 +11,50 @@ source $ROOT/prompt/node.sh
 
 setopt prompt_subst
 
-function noop() {}
+function noop() { }
 
-function my_prompt() {
-  RPROMPT='$(git_line)'
+function render_git_status() {
+  # Escape codes move the git status up one row
+  RPROMPT='%{'$'\e[1A''%}$(git_line)%{'$'\e[1B''%}'
 
   zle && zle .reset-prompt
   async_stop_worker prompt_worker -n
 }
 
-async_load_prompt() {
+function async_load_rprompt() {
   async_init
   async_start_worker prompt_worker -n
-  async_register_callback prompt_worker my_prompt
+  async_register_callback prompt_worker render_git_status
   async_job prompt_worker noop
 
   RPROMPT="…"
 }
 
 # Redraw prompt on terminal focus (requires focus reporting)
-printf '\e[?1004h'
+function _enable_focus_reporting() { printf '\e[?1004h'; }
+function _disable_focus_reporting() { printf '\e[?1004l'; }
 
 function _terminal_focus_in() {
   zle && zle .reset-prompt
 }
-
 function _terminal_focus_out() { }
 
-zle -N _terminal_focus_in
-zle -N _terminal_focus_out
-
-bindkey '\e[I' _terminal_focus_in
-bindkey '\e[O' _terminal_focus_out
-
 function precmd() {
+  _enable_focus_reporting
+
   PROMPT='$(border)
 $(current_dir) $(node_version) $(jobbies)
 $(exit_status)${VIM_MODE}${RESET} '
   RPROMPT=''
 
-  async_load_prompt
+  async_load_rprompt
 }
+
+function preexec() { _disable_focus_reporting; }
+
+zle -N _terminal_focus_in
+zle -N _terminal_focus_out
+bindkey '\e[I' _terminal_focus_in
+bindkey '\e[O' _terminal_focus_out
+
+_enable_focus_reporting
