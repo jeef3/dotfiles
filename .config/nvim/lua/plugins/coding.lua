e832dae5ef8e8cb620c6e5a2e3e6d3b52eda6ecf
Jeff Knaggs <mail@jeef3.com>
2026-05-08 14:02:12 +1200

    Remove this for now

--- .config/nvim/lua/plugins/coding.lua
+++ .config/nvim/lua/plugins/coding.lua
@@ -67,16 +67,6 @@ return {
             module = "blink-cmp-copilot",
             score_offset = 100,
             async = true,
-            transform_items = function(_, items)
-              local CompletionItemKind =
-                require("blink.cmp.types").CompletionItemKind
-              local kind_idx = #CompletionItemKind + 1
-              CompletionItemKind[kind_idx] = "Copilot"
-              for _, item in ipairs(items) do
-                item.kind = kind_idx
-              end
-              return items
-            end,
           },
         },
       },
