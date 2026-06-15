61e11dc8d979d84c2e08fd7b1d9d4580a8c1f521
Jeff Knaggs <mail@jeef3.com>
2026-06-15 14:40:35 +1200

    Add emoji finder

--- .config/nvim/lua/plugins/coding.lua
+++ .config/nvim/lua/plugins/coding.lua
@@ -203,4 +203,32 @@ return {
       },
     },
   },
+
+  ------------------
+  -- nerdy.nvim
+  --
+  -- Find Nerd Glyphs Easily 🤓🔭
+  --
+  -- https://github.com/2KAbhishek/nerdy.nvim
+  ------------------
+  {
+    "2kabhishek/nerdy.nvim",
+    dependencies = {
+      "folke/snacks.nvim",
+    },
+    cmd = "Nerdy",
+    opts = {
+      max_recents = 30, -- Configure recent icons limit
+      copy_to_clipboard = false, -- Copy glyph to clipboard instead of inserting
+      copy_register = "+", -- Register to use for copying (if `copy_to_clipboard` is true)
+    },
+    keys = {
+      { "<leader>in", "<cmd>Nerdy list<CR>", desc = "Browse nerd icons" },
+      {
+        "<leader>iN",
+        "<cmd>Nerdy recents<CR>",
+        desc = "Browse recent nerd icons",
+      },
+    },
+  },
 }
