89fa6997a30d6261f55584419acf3033ff58638a
Jeff Knaggs <mail@jeef3.com>
2026-06-27 22:21:33 +1200

    Removing some I don't use anymore

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -10,26 +10,12 @@ return {
   "tpope/vim-vinegar", -- Netrw enhancements
   "tpope/vim-repeat", -- Get more use out of "."
   "tpope/vim-sleuth", -- Set shiftwidth and expandtab based on current file
-  -- "tpope/vim-commentary", -- gcc to comment line/paragraph
   "tpope/vim-surround", -- Change surrounds, quotes etc
   "tpope/vim-fugitive", -- Git wrapper, :Gstatus etc
 
   "Xvezda/vim-readonly", -- Lock a bunch of files like node_modules
 
-  "fladson/vim-kitty", -- Kitty config syntax
   "MunifTanjim/nui.nvim", -- UI Library
-  -- "justinmk/vim-sneak", -- Minimal EasyMotion s
-  -- {
-  --   "ggandor/leap.nvim",
-  --   lazy = false,
-  --   config = function()
-  --     -- require("leap").create_default_mappings()
-  --     vim.keymap.set("n", "s", "<Plug>(leap)")
-  --     vim.keymap.set("n", "S", "<Plug>(leap-from-window)")
-  --
-  --     require("leap").opts.highlight_unlabeled_phase_one_targets = true
-  --   end,
-  -- },
 
   {
     "kevinhwang91/nvim-hlslens",
@@ -101,62 +87,29 @@ return {
       autoload = true,
       use_git_branch = true,
     },
-    init = function()
-      local group = vim.api.nvim_create_augroup("PersistedHooks", {})
-
-      local function get_cwd_as_name()
-        local dir = vim.fn.getcwd(0)
-        return dir:gsub("[^A-Za-z0-9]", "_")
-      end
-
-      -- vim.api.nvim_create_autocmd({ "User" }, {
-      --   pattern = "PersistedSavePre",
-      --   group = group,
-      --   callback = function(a)
-      --     vim.notify("Save session" .. a.data)
-
-      --     local overseer = require("overseer")
-      --     overseer.save_task_bundle(
-      --       get_cwd_as_name(),
-      --       nil,
-      --       { on_conflict = "overwrite" }
-      --     )
-      --   end,
-      -- })
-
-      -- vim.api.nvim_create_autocmd({ "User" }, {
-      --   pattern = "PersistedLoadPost",
-      --   group = group,
-      --   callback = function(session)
-      --     -- if session == nil or session.name == nil then
-      --     --   return
-      --     -- end
-
-      --     vim.notify("Loaded session " .. vim.inspect(session))
-      --   end,
-      -- })
-    end,
   },
 
+  ------------------
+  -- pets.nvim
+  --
+  -- Pets.nvim is a plugin that provides the missing core functionality of
+  -- showing little animal friends inside your editor.
+  --
+  -- https://github.com/giusgad/pets.nvim
+  ------------------
   {
-    "piersolenski/wtf.nvim",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "MunifTanjim/nui.nvim",
-      "folke/snacks.nvim",
-    },
+    "giusgad/pets.nvim",
+    dependencies = { "MunifTanjim/nui.nvim", "giusgad/hologram.nvim" },
     opts = {},
   },
 
-  {
-    enabled = true,
-    "giusgad/pets.nvim",
-    dependencies = { "MunifTanjim/nui.nvim", "giusgad/hologram.nvim" },
-    opts = {
-      row = 9,
-    },
-  },
-
+  ------------------
+  -- image.nvim
+  --
+  -- 🖼️ Bringing images to Neovim.
+  --
+  -- https://github.com/3rd/image.nvim
+  ------------------
   {
     "3rd/image.nvim",
     build = false,
@@ -165,6 +118,13 @@ return {
     },
   },
 
+  ------------------
+  -- worktrees.nvim
+  --
+  -- Git worktree wrapper for neovim
+  --
+  -- https://github.com/Juksuu/worktrees.nvim
+  ------------------
   {
     "Juksuu/worktrees.nvim",
     keys = {
