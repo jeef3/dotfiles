a0ff0d7afb6824db75385575b116979abf82da35
Jeff Knaggs <mail@jeef3.com>
2026-07-09 09:50:30 +1200

    Remove unused aliases

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -4,18 +4,11 @@ alias showdesktop="defaults write com.apple.finder CreateDesktop -bool true && k
 alias hd="hidedesktop; hide"
 alias sd="showdesktop; show"
 
-# App shortcuts
-alias t="todo.sh"
-alias todo="todotxt-machine ./todo.txt"
-
 alias lg="lazygit"
 
 alias urldecode='python3 -c "import sys, urllib.parse; print(urllib.parse.unquote(sys.argv[1]))"'
 
 alias urlencode='python3 -c "import sys, urllib.parse; print(urllib.parse.quote(sys.argv[1]))"'
-
-
-alias du="ncdu --color dark -rr --exclude .git --exclude node_modules"
 
 alias ssh="TERM=xterm ssh"
 
