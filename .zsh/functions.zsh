681740f9fa724d5208afc3d294c7e0073bd4be28
Jeff Knaggs <mail@jeef3.com>
2026-08-05 13:37:53 +1200

    Worktree helpers

--- .zsh/functions.zsh
+++ .zsh/functions.zsh
@@ -18,3 +18,32 @@ function _up-dir {
 }
 zle -N _up-dir
 bindkey "^h" _up-dir
+
+function wtcd {
+  if [[ -n "${TMUX:-}" && -n "${TMUX_PANE:-}" ]] &&
+    tmux display-message -p -t "$TMUX_PANE" '#{pane_id}' >/dev/null 2>&1; then
+    command wtcd
+    return
+  fi
+
+  local destination
+  destination="$(command wtcd)" || return
+
+  if [[ -n "$destination" && -d "$destination" ]]; then
+    cd -- "$destination"
+  fi
+}
+
+function _change-to-worktree {
+  local destination
+  destination="$(tmux show-option -pv -t "$TMUX_PANE" @wtcd-destination 2>/dev/null)"
+
+  if [[ -n "$destination" && -d "$destination" ]]; then
+    cd -- "$destination"
+    zle reset-prompt
+  fi
+}
+zle -N _change-to-worktree
+for keymap in emacs viins vicmd; do
+  bindkey -M "$keymap" "^X^W" _change-to-worktree
+done
