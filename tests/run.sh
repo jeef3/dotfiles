d0a829501e899b6ce743954ca54b227b9be7ede4
Jeff Knaggs <mail@jeef3.com>
2026-09-04 15:25:39 +1200

    Just tweak the test order slightly

--- tests/run.sh
+++ tests/run.sh
@@ -5,11 +5,12 @@ export TERM="${TERM:-xterm-256color}"
 
 test_dir="$(cd "$(dirname "$0")" && pwd)"
 
-# Bootstrap and setup run first: setup installs Neovim, which the Neovim
-# tests then exercise.
+# Unit tests
 "$test_dir/test-bootstrap.sh"
 "$test_dir/test-setup.sh"
-"$test_dir/test-spinner.zsh"
 "$test_dir/test-nvim.sh"
-"$test_dir/test-util.sh"
 "$test_dir/test-zsh.sh"
+
+# Visual tests
+"$test_dir/test-spinner.zsh"
+"$test_dir/test-util.sh"
